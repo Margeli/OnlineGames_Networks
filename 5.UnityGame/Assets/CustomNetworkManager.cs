2cc33fb530ff5ad8aa2efd886bb2ddc4945342b2
Margeli <niniopantalones@gmail.com>
2019-12-03 12:44:02 +0100

    "Changing the player prefab after connecting" DONE SO FAR

---
+++ 5.UnityGame/Assets/CustomNetworkManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    public short playerPrefabIndex;
+
+    //1)executed in the server
+    public override void OnStartServer()
+    {
+        NetworkServer.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnPrefabResponse);
+        base.OnStartServer();
+    }
+
+    //2)executed in the server
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        client.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnPrefabRequest);
+        base.OnClientConnect(conn);
+    }
+
+    //3) Executed in the server
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+        msg.controllerId = playerControllerId;
+        NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefabSelect, msg);
+    }
+
+    //4)Prefab requested in the client
+    private void OnPrefabRequest(NetworkMessage netMsg)
+    {
+        MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
+        msg.prefabIndex = playerPrefabIndex;
+        client.Send(MsgTypes.PlayerPrefabSelect, msg);
+    }
+
+    //5)Prefab communicated to the server
+    private void OnPrefabResponse(NetworkMessage netMsg)
+    {
+        MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
+        playerPrefab = spawnPrefabs[msg.prefabIndex];
+        base.OnServerAddPlayer(netMsg.conn, msg.controllerId);
+    }
+
+    public string[] playerNames = new string[] { "Boy", "Girl", "Robot" };
+    private void OnGUI()
+    {
+        if (!isNetworkActive)
+        {
+            playerPrefabIndex = (short)GUI.SelectionGrid(
+                new Rect(Screen.width - 200, 10, 200, 50),
+                playerPrefabIndex,
+                playerNames,
+                3);
+        }
+    }
+}
+
+public class MsgTypes
+{
+    public const short PlayerPrefabSelect = MsgType.Highest + 1;
+    public class PlayerPrefabMsg : MessageBase
+    {
+        public short controllerId;
+        public short prefabIndex;
+    }
+}
